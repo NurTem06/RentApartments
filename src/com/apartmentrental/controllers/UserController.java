ee1efba124b47c673e257eabad930c4c06afb615
NurTem06 <temirgalinurislam73@gmail.com>
2025-01-31 09:17:15 +0500

    base

---
+++ src/com/apartmentrental/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.apartmentrental.controllers;
+
+public class UserController {
+}
---
+++ src/com/apartmentrental/interfaces/UserControllerInterface.java
@@ -0,0 +1,4 @@
+package com.apartmentrental.interfaces;
+
+public interface UserControllerInterface {
+}
