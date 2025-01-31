75d003cc7e7b870f65f2467195babec10cf8b8ca
Kyrmyzy Zholekesh <242247@astanait.edu.kz>
2025-01-31 09:41:34 +0500

    apartments by Kyrmyzy

---
+++ src/models/Apartment.java
@@ -0,0 +1,95 @@
+public class Apartment {
+    private int id;
+    private String name;
+    private String city;
+    private String district;
+    private String street;
+    private int floor;
+    private int rooms;
+    private double pricePerDay;
+    private double pricePerMonth;
+    private double pricePerYear;
+    private String status;
+    private String availabilityDate;
+    private double rating;
+
+    public Apartment() {}
+
+    public Apartment(String name, String city, String district, String street, int floor, int rooms, double pricePerDay, double pricePerMonth, double pricePerYear, String status, String availabilityDate, double rating) {
+        setName(name);
+        setCity(city);
+        setDistrict(district);
+        setStreet(street);
+        setFloor(floor);
+        setRooms(rooms);
+        setPricePerDay(pricePerDay);
+        setPricePerMonth(pricePerMonth);
+        setPricePerYear(pricePerYear);
+        setStatus(status);
+        setAvailabilityDate(availabilityDate);
+        setRating(rating);
+    }
+
+    public Apartment(int id, String name, String city, String district, String street, int floor, int rooms, double pricePerDay, double pricePerMonth, double pricePerYear, String status, String availabilityDate, double rating) {
+        this(name, city, district, street, floor, rooms, pricePerDay, pricePerMonth, pricePerYear, status, availabilityDate, rating);
+        setId(id);
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getDistrict() { return district; }
+    public void setDistrict(String district) { this.district = district; }
+
+    public String getStreet() { return street; }
+    public void setStreet(String street) { this.street = street; }
+
+    public int getFloor() { return floor; }
+    public void setFloor(int floor) { this.floor = floor; }
+
+    public int getRooms() { return rooms; }
+    public void setRooms(int rooms) { this.rooms = rooms; }
+
+    public double getPricePerDay() { return pricePerDay; }
+    public void setPricePerDay(double pricePerDay) { this.pricePerDay = pricePerDay; }
+
+    public double getPricePerMonth() { return pricePerMonth; }
+    public void setPricePerMonth(double pricePerMonth) { this.pricePerMonth = pricePerMonth; }
+
+    public double getPricePerYear() { return pricePerYear; }
+    public void setPricePerYear(double pricePerYear) { this.pricePerYear = pricePerYear; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getAvailabilityDate() { return availabilityDate; }
+    public void setAvailabilityDate(String availabilityDate) { this.availabilityDate = availabilityDate; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", district='" + district + '\'' +
+                ", street='" + street + '\'' +
+                ", floor=" + floor +
+                ", rooms=" + rooms +
+                ", pricePerDay=" + pricePerDay +
+                ", pricePerMonth=" + pricePerMonth +
+                ", pricePerYear=" + pricePerYear +
+                ", status='" + status + '\'' +
+                ", availabilityDate='" + availabilityDate + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
---
+++ src/repositories/ApartmentRepository.java
@@ -0,0 +1,42 @@
+import com.apartmentrental.data.PostgresDB;
+import com.apartmentrental.models.Apartment;
+import com.apartmentrental.repositories.interfaces.IApartmentRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApartmentRepository implements IApartmentRepository {
+    PostgresDB postgresDB = new PostgresDB("localhost:5432", "postgres","Nurislam2006","RentApartments");
+    @Override
+    public List<Apartment> getAllApartments() {
+        List<Apartment> apartments = new ArrayList<>();
+        String sql = "SELECT * FROM apartments";
+        try (Connection conn = postgresDB.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                apartments.add(new Apartment(rs.getString("name"), rs.getString("city"), rs.getString("district"), rs.getString("street"), rs.getInt("floor"), rs.getInt("rooms"), rs.getDouble("price_day"), rs.getDouble("price_month"), rs.getDouble("price_year"), rs.getString("status"), rs.getString("availability_date"), rs.getInt("id")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return apartments;
+    }
+    @Override
+    public List<Apartment> findApartmentByName(String name) {
+        List<Apartment> apartments = new ArrayList<>();
+        String sql = "SELECT * FROM apartments WHERE name ILIKE ?";
+        try (Connection conn = postgresDB.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, "%" + name + "%"); // Поиск по части названия
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                apartments.add(new Apartment(rs.getString("name"), rs.getString("city"), rs.getString("district"), rs.getString("street"), rs.getInt("floor"), rs.getInt("rooms"), rs.getDouble("price_day"), rs.getDouble("price_month"), rs.getDouble("price_year"), rs.getString("status"), rs.getString("availability_date"), rs.getInt("id")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return apartments;
+    }
+}
