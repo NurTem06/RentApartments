eda4ec5cdd0aae73f268589bc5bb23be1a1b74c8
NurTem06 <temirgalinurislam73@gmail.com>
2025-01-31 09:44:06 +0500

    base

--- src/repositories/interfaces/IUserRepository.java
+++ src/repositories/interfaces/IUserRepository.java
@@ -1,4 +1,8 @@
-package repositories.interfaces;
+package com.apartmentrental.repositories.interfaces;
+
+import com.apartmentrental.models.User;
 
 public interface IUserRepository {
+    void registerUser(String firstName, String lastName, String phone, String username, String password);
+    User loginUser(String username, String password);
 }
