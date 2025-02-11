daf5496d55fcc7274d9e35ca2b9642e48a24c218
Kyrmyzy Zholekesh <242247@astanait.edu.kz>
2025-02-11 08:38:33 +0500

    исправила расположение

--- src/com/apartmentrental/controllers/ApartmentController.java
+++ src/com/apartmentrental/controllers/ApartmentController.java
@@ -1,38 +1,55 @@
 package com.apartmentrental.controllers;
-
 import com.apartmentrental.models.Apartment;
 import com.apartmentrental.repositories.ApartmentRepository;
+import com.apartmentrental.views.ConsoleView;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class ApartmentController {
     private final ApartmentRepository apartmentRepository = new ApartmentRepository();
-
+    private final ConsoleView view = new ConsoleView();
     public void listApartments() {
         List<Apartment> apartments = apartmentRepository.getAllApartments();
+
         if (apartments.isEmpty()) {
-            System.out.println("Нет доступных квартир.");
-        } else {
-            for (Apartment apartment : apartments) {
-                System.out.println(apartment.getId() + ". " + apartment.getName() + " - " +
-                        apartment.getCity() + ", " + apartment.getDistrict() + ", " + apartment.getStreet() +
-                        " | Этаж: " + apartment.getFloor() + " | Комнат: " + apartment.getRooms() +
-                        " | Цена за день: " + apartment.getPricePerDay() + " | Цена за месяц: " +
-                        apartment.getPricePerMonth() + " | Цена за год: " + apartment.getPricePerYear() +
-                        " | Доступность: " + apartment.getStatus());
+            view.displayMessage("Нет доступных квартир.");
+            return;
+        }
+
+        view.displayMessage("Доступные квартиры:");
+        view.displayMessage("Категория: Эконом (до 5000 тг/день)");
+        for (Apartment apartment : apartments) {
+            if (apartment.getPricePerDay() < 5000) {
+                displayApartmentDetails(apartment);
+            }
+        }
+
+        view.displayMessage("\nКатегория: Бизнес (от 5000 тг/день и выше)");
+        for (Apartment apartment : apartments) {
+            if (apartment.getPricePerDay() >= 5000) {
+                displayApartmentDetails(apartment);
             }
         }
     }
 
+    private void displayApartmentDetails(Apartment apartment) {
+        view.displayMessage(apartment.getId() + ". " + apartment.getName() + " - " +
+                apartment.getCity() + ", " + apartment.getDistrict() + ", " +
+                apartment.getStreet() + " | Этаж: " + apartment.getFloor() +
+                " | Комнат: " + apartment.getRooms() + " | Цена за день: " +
+                apartment.getPricePerDay() + " | Цена за месяц: " +
+                apartment.getPricePerMonth() + " | Цена за год: " +
+                apartment.getPricePerYear() + " | Доступность: " +
+                apartment.getStatus());
+    }
+
     public void searchApartmentByName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите название квартиры: ");
-        String name = scanner.nextLine();
+        String name = view.promptForString("Введите название квартиры: ");
 
         List<Apartment> apartments = apartmentRepository.findApartmentByName(name);
         if (apartments.isEmpty()) {
-            System.out.println("Квартира не найдена.");
+            view.displayMessage("Квартира не найдена");
         } else {
             for (Apartment apartment : apartments) {
                 System.out.println(apartment.getId() + ". " + apartment.getName() + " - " +
